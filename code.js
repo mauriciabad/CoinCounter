a0e869b7d1233642726f16e97acc1a27512687bd
ferjappy <ferranvelasco98@gmail.com>
2017-11-12 00:53:37 +0100

    code finished almost

--- code.js
+++ code.js
@@ -1,31 +1,23 @@
 $(".coin-img").draggable({ revert: true});
 
-<<<<<<< HEAD
 
 $("#wallet").droppable({
-    accept: ".coin-img";
+    accept: ".coin-img",
     drop: function(event,ui){
         var new_coin = $(ui.helper).clone();
         $("#wallet").append(new_coin);
     }
 });
-=======
-$( "#droppable" ).droppable({
-	drop: function( event, ui ) {
-		$( this )
-			.addClass( "ui-state-highlight" )
-			.find( "p" )
-			.html( "Dropped!" );
-	}
-});
+
 
 
 
 
 var total = 0.00;
+var coins = {2: 0, 1: 0, 0.5: 0, 0.2: 0, 0.1: 0, 0.05: 0, 0.02: 0, 0.01: 0};
 
 function add(value){
+	coins[value]++;
 	total += value * 0.8843442;
 	document.getElementById('display-value').innerHTML = "£" + total.toFixed(2);
 }
->>>>>>> 557cd1d41f94e75306be0d267aed1d0100af20ba
