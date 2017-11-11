c59637069039a29ec222196ed4fc14737b9f6f9b
ferjappy <ferranvelasco98@gmail.com>
2017-11-11 23:42:09 +0100

    drag and drop v2

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
 $(".coin-img").draggable({ revert: true});
 
-$( "#droppable" ).droppable({
-	drop: function( event, ui ) {
-		$( this )
-			.addClass( "ui-state-highlight" )
-			.find( "p" )
-			.html( "Dropped!" );
-	}
+
+$("#wallet").droppable({
+    accept: ".coin-img",
+    drop: function(event,ui){
+        var new_coin = $(ui.helper).clone();
+        new_coin.draggable();
+        $(this).append(new_coin);
+    }
 });
