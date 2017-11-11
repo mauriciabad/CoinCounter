3b33b939e0642eccf30afeb5f141f2455085b230
mauriciabad <mauriciabad@yahoo.es>
2017-11-12 00:29:49 +0100

    code.js corrected

--- code.js
+++ code.js
@@ -1,22 +1,11 @@
 $(".coin-img").draggable({ revert: true});
 
-<<<<<<< HEAD
-
 $("#wallet").droppable({
     accept: ".coin-img";
     drop: function(event,ui){
         var new_coin = $(ui.helper).clone();
         $("#wallet").append(new_coin);
     }
-});
-=======
-$( "#droppable" ).droppable({
-	drop: function( event, ui ) {
-		$( this )
-			.addClass( "ui-state-highlight" )
-			.find( "p" )
-			.html( "Dropped!" );
-	}
 });
 
 
@@ -28,4 +17,3 @@ function add(value){
 	total += value * 0.8843442;
 	document.getElementById('display-value').innerHTML = "£" + total.toFixed(2);
 }
->>>>>>> 557cd1d41f94e75306be0d267aed1d0100af20ba
