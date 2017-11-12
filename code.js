eafa5f4e92f4bfac003423092a4d088f87d1ed3a
ferjappy <ferranvelasco98@gmail.com>
2017-11-12 01:51:59 +0100

    puto codej2

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ $("#wallet").droppable({
     accept: ".coin-img",
     drop: function(event,ui){
         var new_coin = $(ui.helper).clone();
-        $("#wallet").append(new_coin);
+        document.getElementById("#wallet").appendChild(new_coin);
     }
 });
 
