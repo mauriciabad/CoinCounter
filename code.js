f9b5e45a36c94143c395c5f36d4011f73c09108e
mauriciabad <mauriciabad@yahoo.es>
2017-11-12 01:10:18 +0100

    ; to ,

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 $(".coin-img").draggable({ revert: true});
 
 $("#wallet").droppable({
-    accept: ".coin-img";
+    accept: ".coin-img",
     drop: function(event,ui){
         var new_coin = $(ui.helper).clone();
         $("#wallet").append(new_coin);
